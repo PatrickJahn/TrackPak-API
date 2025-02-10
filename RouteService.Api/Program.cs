fd437d543fd93f1e29c0516a64966c1d48e58124
Casper  Sejrup <sejrup112@gmail.com>
2025-02-10 17:03:30 +0100

    route service solution.

---
+++ RouteService.Api/Program.cs
@@ -0,0 +1,22 @@
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+//builder.Services.AddInfrastructure(builder.Configuration);
+//builder.Services.AddScoped<IOrderService, OrderService.Application.Services.OrderService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+//app.MapRouteEndpoints();
+app.Run();
---
+++ RouteService.Application/DependencyInjection.cs
@@ -0,0 +1,6 @@
+namespace RouteService.Application;
+
+public class DependencyInjection
+{
+    
+}
